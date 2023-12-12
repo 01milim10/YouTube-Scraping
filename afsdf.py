e0e323f26791b16769647892a1ac457194cb438a
Milan Budhathoki <mbudhathoki1996@gmail.com>
2023-12-11 18:35:59 -0600

    Removed unwanted files

--- afsdf.py
+++
@@ -1,9 +0,0 @@
-# Open a file in write mode with UTF-8 encoding
-with open('output.txt', 'w', encoding='utf-8') as file:
-    # String containing characters to be written
-    text = "Hello, 你好, स्वागत हैं, বাংলা ভাষা"
-
-    # Write the string to the file
-    file.write(text)
-
-print("Characters have been written to 'output.txt' using UTF-8 encoding.")
